0fcf7e05ed41729ee93376da4db4decd0427ef1f
Iqra <105969934+ippirate@users.noreply.github.com>
2026-01-31 20:51:49 +0500

    Refactor popup.js to include time formatting and updates
    lolz

--- popup.js
+++ popup.js
@@ -9,9 +9,32 @@ document.getElementById("lock").addEventListener("click", () => {
   });
 });
 
+
+
 document.getElementById("unlock").addEventListener("click", () => {
   chrome.storage.local.set({
     lockMode: false,
     allowedTabs: []
   });
 });
+
+function formatTime(ms) {
+  const seconds = Math.floor(ms / 1000)
+  const minutes = Math.floor (seconds / 60)
+  return `${minutes}:${String(seconds % 60).padStart(2, "0")}`;
+}
+
+function updateTime(){
+  chrome.runtime.sendMessage(
+    { type: "GET_ACTIVE_TAB_TIME"},
+    (response) => {
+      if (!response) return;
+      document.getElementById("time").textContent = 
+      formatTime(response.time)
+    }
+  )
+}
+
+
+setInterval(updateTime, 1000);
+updateTime();
